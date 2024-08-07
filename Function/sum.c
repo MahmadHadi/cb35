324ca0f48c3665513f6b7b889a64d141c5bbd039
MahmadHadi <nayanihadi97@gmail.com>
2024-08-07 19:59:10 +0530

    7 aug

---
+++ Function/sum.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int getSum(int number1, int number2) {
+    int sum = number1 + number2;
+    return sum;
+}
+
+int main() { 
+    int num1 = 1, num2 = 2;
+    int ans = getSum(num1, num2);
+
+    printf("ans = %d ", ans);
+    return 0;
+}
+
+// return type 
+//     void 
+//     int 
+//     float 
+//     char 
+//     bool
---
+++ Function/sumOfTwoNums.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main() {
+    // int num1 = 1, num2 = 2;
+
+    // int sum = num1 + num2;
+    // printf("sum = %d ", sum);
+    printf("sum = %d ", 1 + 2);
+    printf("sub = %d ", 1 - 2);
+    printf("product = %d ", 1 * 2);
+}
