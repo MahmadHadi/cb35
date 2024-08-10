99d20f979a37cbf3114aefa4c19a7f814f642b14
MahmadHadi <nayanihadi97@gmail.com>
2024-08-10 19:57:33 +0530

    10 aug

--- Function/isPrime.c
+++ Function/isPrime.c
@@ -14,27 +14,50 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-bool isEven(int num) {
-    if num == 1, num == 2 -> true 
-    num % 2 != 0 -> true 
-    else -> false 
+bool isPrime(int num) {
+    for (int i = 2; i <= num - 1; i++) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
 }
-
-num = 2 
-    return true
-
-num = 7 
-num % 2 != 0 -> 7 % 2 != 0 -> t 
-1 3 9 
-num = 9, 21, 81,
-num % 2 != 0 -> 9 % 2 != 0 -> t 
 
 int main() { 
 
-    int num;
-    printf("Enter a number : ");
-    scanf("%d", &num);
+    int num = 11;
+    // printf("Enter a number : ");
+    // scanf("%d", &num);
 
-
+    // printf("%d ", isPrime(num));
+    
+    if (num == 0) {
+        printf("0 is neither prime nor composite ");
+    } else if (isPrime(num)) {
+        printf("Num is prime a num ");
+    } else {
+        printf("Num is not a prime ");
+    }
     return 0;
 }
+
+
+
+// num = 3 
+// isPrime(3)
+
+// i = 2 
+// i <= 2 -> t 
+//     num % 2 == 0 -> 3 % 2 == 0 -> f 
+//     i++ // i = 3
+// i <= 2 -> f -> loop break;
+// return true 
+
+// num = 4 
+
+// i = 2 
+// i <= 3 -> 2 <= 3 -> t 
+//     num % i == 0 -> 4 % 2 == 0 -> t 
+//         return true -> fun break;
+
+// main() 
---
+++ Function/primeBw1ToN.c
@@ -0,0 +1,15 @@
+// num = 5 
+// 1 - 5 
+
+// 1 2 3 5 
+
+// num = 4 
+// 1 2 3 
+
+#include <stdio.h>
+int main() {
+
+    
+
+    return 0;
+}
